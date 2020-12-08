59cc47e69d8c1c53629e3877e167e695db79f8f8
Kotya Shmotoloha <kostiashmotoloha@gmaol.com>
2020-12-08 12:47:06 +0200

    addNew

--- js/index.js
+++ js/index.js
@@ -1,113 +1,134 @@
-function validateEmail(email) {
-  let re = /\S+@\S+\.\S+/;
-  return re.test(email);
+$(document).ready(function () {
+    $('body').removeClass('preload');
+    var touchmoved = false,
+        compare_counter = 0,
+        favorite_counter = 0;
+
+    $('.menu-wrapper').PopupSlider({
+        effect: 'left',
+        button: '.nav-trigger',
+        closeButton: false,
+        container: '.header',
+        breakpoint: 1025,
+        onMenuOpen: false,
+        onMenuClose: false
+    });
+    $('.search select').select2({
+        minimumResultsForSearch: -1,
+        dropdownParent: $('.search')
+    });
+    $('.inner-tabs select').select2({
+        minimumResultsForSearch: -1
+    });
+    $('.level-1-title').on('click touchend', function () {
+        if (touchmoved !== true) {
+            $(this).find('.level-1').addClass('active');
+            return false;
+        }
+    }).on('touchmove', function () {
+        touchmoved = true;
+    }).on('touchstart', function () {
+        touchmoved = false;
+    });
+    $('.level-1').each(function () {
+        $(this).find('.back').on('click touchend', function () {
+            if (touchmoved !== true) {
+                $(this).parent().removeClass('active');
+                return false;
+            }
+        }).on('touchmove', function () {
+            touchmoved = true;
+        }).on('touchstart', function () {
+            touchmoved = false;
+        });
+    });
+    $('.tabs,.inner-tabs').each(function () {
+        var tabContainers = $(this).children('div'),
+            listItem = $(this).find('>.tab-list>li');
+        tabContainers.hide().filter(':first').show();
+        $(this).find('>.tab-list a').click(function () {
+            tabContainers.hide();
+            tabContainers.filter(this.hash).show();
+            $(listItem).removeClass('active');
+            $(this).parent('li').addClass('active');
+            return false;
+        }).filter(':first').click();
+    });
+    $('[id*="filter-"]').each(function () {
+        $(this).find('.reset').on('click touchend', function () {
+            $(this).parents('[id*="filter-"]').find('select').select2("val", '0');
+            $(this).parents('[id*="filter-"]').find('input').val('');
+        });
+    })
+    $('#reset').click(function () {
+
+    });
+    $(document).on('focus', '[id*="filter-"] input', function () {
+        $('[id*="filter-"] input').ForceNumericOnly();
+    });
+
+    $(document).on('click touchend', '.item-compare', function () {
+        if (touchmoved !== true) {
+            if (!$(this).parent().parent().hasClass('compared')) {
+                $(this).parent().parent().addClass('compared');
+                $(this).find('span').text('В сравнении');
+                compare_counter++;
+                if (compare_counter > 0) {
+                    $('#comparison .counter').removeClass('hidden').text(compare_counter);
+                }
+            }
+        }
+    }).on('touchmove', function () {
+        touchmoved = true;
+    }).on('touchstart', function () {
+        touchmoved = false;
+    });
+
+    $(document).on('click touchend', '.item-favorite', function () {
+        if (touchmoved !== true) {
+            if (!$(this).parent().parent().hasClass('in-favorite')) {
+                $(this).parent().parent().addClass('in-favorite');
+                $(this).find('span').text('В избранном');
+                favorite_counter++;
+                if (favorite_counter > 0) {
+                    $('#favorites .counter').removeClass('hidden').text(favorite_counter);
+                }
+            }
+        }
+    }).on('touchmove', function () {
+        touchmoved = true;
+    }).on('touchstart', function () {
+        touchmoved = false;
+    });
+
+    jQuery.fn.ForceNumericOnly =
+        function () {
+            let ua = navigator.userAgent.toLowerCase();
+            let isAndroid = ua.indexOf("android") > -1;
+            return this.each(function () {
+                if (isAndroid) {
+                    $(this).attr('type', 'number');
+                } else {
+                    var regex = /([0-9])/g;
+                    $(this).on('keydown keypress', function (e) {
+                        if (!(e.key.match(regex) || e.key === "Backspace" || e.key === "ArrowRight" || e.key === "ArrowLeft" || e.key === "Delete")) {
+                            e.preventDefault();
+                        }
+                    });
+                }
+            });
+        };
+    $('#phone').mask("+38(099)999-99-99");
+});
+
+function recallSubmit() {
+    $.fancybox.close();
+    $.fancybox.open('Спасибо! Мы вам перезвоним!');
 }
-const btn = document.querySelector(".btn-folov");
-function validate() {
-  const email = document.getElementById("email");
-  const emailVal = email.value;
 
-  if (validateEmail(emailVal)) {
-    const modaFolov = document.querySelector(".email-modal");
-    modaFolov.classList.add("text-block");
-    setTimeout(() => {
-      modaFolov.classList.remove("text-block");
-    }, 1500);
-  } else {
-    const text = document.querySelector(".text-none");
-    text.classList.add("text-block");
-    setTimeout(() => {
-      text.classList.remove("text-block");
-    }, 1500);
-  }
-  return false;
+function letterSpam() {
+    $.fancybox.open('Вы успешно подписаны на рассылку!');
+    setTimeout(function () {
+        $.fancybox.close();
+    }, 2000);
 }
-
-btn.addEventListener("click", validate);
-
-const inputNum1 = document.querySelector("#number-1");
-const inputNum2 = document.querySelector("#number-2");
-function validateForm1() {
-  if (inputNum1.value.match(/^\d+/)) {
-    inputNum1.classList.remove("red");
-  } else {
-    inputNum1.classList.add("red");
-  }
-}
-inputNum1.addEventListener("input", validateForm1);
-
-function validateForm2() {
-  if (inputNum2.value.match(/^\d+/)) {
-    inputNum2.classList.remove("red");
-  } else {
-    inputNum2.classList.add("red");
-  }
-}
-inputNum2.addEventListener("input", validateForm2);
-
-const password = document.querySelector("#password");
-function valiPasswords() {
-  if (password.value.length > 3) {
-    password.classList.remove("red");
-  } else {
-    password.classList.add("red");
-  }
-}
-
-password.addEventListener("input", valiPasswords);
-
-const search = document.querySelector("#search");
-function valiSearch() {
-  if (search.value.length > 2) {
-    search.classList.remove("red");
-  } else {
-    search.classList.add("red");
-  }
-}
-
-search.addEventListener("input", valiSearch);
-function form_submitHandler({ target }) {
-  target.reset();
-}
-let form = document.querySelector("#btn-filter");
-
-form.addEventListener("click", form_submitHandler);
-function openModal() {
-  const enterModal = document.querySelector(".enter-modal");
-  enterModal.classList.add("text-block");
-}
-const btnLogin = document.querySelector(".btn-login");
-
-btnLogin.addEventListener("click", openModal);
-function closeModal() {
-  const enterModal = document.querySelector(".enter-modal");
-  enterModal.classList.remove("text-block");
-}
-
-const close = document.querySelector(".close");
-close.addEventListener("click", closeModal);
-
-const itemCompare = document.querySelector(".item-compare");
-let counerCompore = 0;
-
-const counter1 = document.querySelector(".counter1");
-function counerCompore1() {
-  counerCompore++;
-
-  counter1.classList.add("text-block");
-  counter1.innerText = counerCompore;
-}
-const riseProduct = document.querySelector(".rise-product");
-riseProduct.addEventListener("click", counerCompore1);
-
-let counerFavorite = 0;
-
-const counter2 = document.querySelector(".counter2");
-function counerFavorite1() {
-  counerFavorite++;
-  counter2.classList.add("text-block");
-  counter2.innerText = counerFavorite;
-}
-
-const favorite = document.querySelector(".favorite");
-favorite.addEventListener("click", counerFavorite1);
